951ba7226b38db055406724671c79d40855201b4
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2022-11-09 19:32:43 -0500

    improved the notebook and the model cleaner

--- src/model_cleaner.py
+++ src/model_cleaner.py
@@ -7,21 +7,31 @@ if __name__ == '__main__':
   model = tf.keras.models.load_model('./src/labeling_model.h5')
   labels = ['back', 'front', 'other']
 
-  files = glob.glob('./src/downloads2/*.png')
+  files = glob.glob('./src/downloads/*.png')
+  files_to_use = np.random.choice(files, 4000, replace=False)
 
   # test model prediction
-  for idx, image_filename in enumerate(files):
+  for idx, image_filename in enumerate(files_to_use):
     name_file = image_filename.split('/')[-1]
     img = tf.keras.preprocessing.image.load_img(image_filename, target_size=(256, 256))
-    img_array = tf.keras.preprocessing.image.img_to_array(img) / 255.0
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
     pred = model.predict(img_array.reshape(1, 256, 256, 3))
+    thresholld = 0.7
+    idx_pred = None
+    if pred[0] > thresholld:
+      idx_pred = 0
+    elif pred[1] > thresholld:
+      idx_pred = 1
+    else:
+      idx_pred = 2
+    
     label_pred = labels[np.argmax(pred)]
 
     new_path = './src/images/' + label_pred + '/' + name_file
 
     # copy image to new folder
-    shutil.copy(image_filename, new_path)
+    shutil.move(image_filename, new_path)
 
-    print(f'Image {image_filename} predicted as {label_pred}. {len(files) - idx} images left.')
+    print(f'Image {image_filename} predicted as {label_pred}. {len(files_to_use) - idx} images left.')
 
 
--- src/webp_to_png.py
+++ src/webp_to_png.py
@@ -1,11 +1,18 @@
 from PIL import Image
+import imghdr
 import glob
 
 images_files = glob.glob("images/*/*.png")
 
+image_extensions = [".png", ".jpg"]
+img_type_accepted_by_tf = ["bmp", "gif", "jpeg", "png"]
 for idx, img_file in enumerate(images_files):
-  # print(img_file)
-  im = Image.open(img_file).convert('RGB')
-  im.save(img_file, 'png')
-  print(f'Image done {img_file} - {len(images_files) - idx - 1} left.')
+  img_type = imghdr.what(img_file)
+  if img_type is None:
+      print(f"{img_file} is not an image")
+  elif img_type not in img_type_accepted_by_tf:
+      print(f"{img_file} is a {img_type}, not accepted by TensorFlow")
+      im = Image.open(img_file).convert('RGB')
+      im.save(img_file, 'png')
+      print(f'Image done {img_file} - {len(images_files) - idx - 1} left.')
 
