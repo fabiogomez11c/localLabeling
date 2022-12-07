25cd8086952b3f8097a3b127533ff8aadb488fcc
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2022-12-07 14:26:07 -0500

    update

--- cp_file.py
+++ cp_file.py
@@ -5,11 +5,11 @@ import shutil
 import numpy as np
 
 # source folder
-src = './images/bf/back/'
-destination = './images/bf/validation/back'
+src = './images/oi/train/outside/'
+destination = './images/oi/borrar/'
 
 # list all files in the source folder
-files_to_copy = np.random.choice(np.array(glob.glob(src + '*.png')), 200, replace=False)
+files_to_copy = np.random.choice(np.array(glob.glob(src + '*.png')), 1000, replace=False)
 for file in files_to_copy:
     shutil.move(file, destination)
 
