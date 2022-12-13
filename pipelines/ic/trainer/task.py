cf7a5b54df16a29a28a983c0e740fea7491f8b7d
Fabio Gomez <fabiogomez11c@gmail.com>
2022-12-13 14:01:29 +0000

    added download_images to use gsutil

--- pipelines/ic/trainer/task.py
+++ pipelines/ic/trainer/task.py
@@ -1,7 +1,21 @@
+import logging
+import tensorflow as tf
 
-# import the data
+from get_images import download_images
+
+BATCH_SIZE = 32
+IMAGE_SIZE = (256, 256)
+CLASS_NAMES = ['incorrect', 'correct']
+
+logging.info(f'List GPUs: {tf.config.list_physical_devices("GPU")}')
+
+# get images
+logging.info('Downloading images')
+download_images('gs://mom_seguros_images_car/ic/', 'ic/')
 
 # create the dataset
+logging.info(f'Creating datasets for training')
+
 
 # create and compile the model
 
