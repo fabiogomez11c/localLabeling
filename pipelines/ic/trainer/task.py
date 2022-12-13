3674078e2e3a4d858760dac687596a2135e0fa21
Fabio Gomez <fabiogomez11c@gmail.com>
2022-12-13 14:22:04 +0000

    added data augmentation layer and prefecth

--- pipelines/ic/trainer/task.py
+++ pipelines/ic/trainer/task.py
@@ -6,6 +6,7 @@ from get_images import download_images
 BATCH_SIZE = 32
 IMAGE_SIZE = (256, 256)
 CLASS_NAMES = ['incorrect', 'correct']
+AUTOTUNE = tf.data.AUTOTUNE
 
 logging.info(f'List GPUs: {tf.config.list_physical_devices("GPU")}')
 
@@ -29,15 +30,17 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=IMAGE_SIZE,
   batch_size=BATCH_SIZE
 )
-test_ds = tf.keras.utils.image_dataset_from_directory(
-  'ic/test',
-  class_names=CLASS_NAMES,
-  shuffle=False,
-  image_size=IMAGE_SIZE,
-  batch_size=BATCH_SIZE
-)     
+# data augmentation layer
+data_augmentation = tf.keras.Sequential([
+  tf.keras.layers.RandomFlip("horizontal_and_vertical"),
+  tf.keras.layers.RandomRotation(0.2),
+])
+
+train_ds = train_ds.prefetch(AUTOTUNE)
+val_ds = val_ds.prefetch(AUTOTUNE)
 
 # create and compile the model
+logging.info('Creating and compiling the model')
 
 # train the model
 
