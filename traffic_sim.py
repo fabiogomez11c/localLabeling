709faf6a53340c652dd1aedd885999be8ca3433c
Fabio Gomez <fabiogomez11c@gmail.com>
2022-12-30 18:25:10 +0000

    added traffic sim results

--- traffic_sim.py
+++ traffic_sim.py
@@ -1,16 +1,26 @@
 import requests
+import time
+import pandas as pd
 
-def request_test():
+def request_test(x):
   url = 'https://us-east1-mom-seguros.cloudfunctions.net/classify_ic'
   body = {}
 
+  now = time.time()
   x = requests.post(url, json = body)
+  try:
+    result = {**x.json()}
+  except:
+    result = {'prediction': 'error'}
+  result['exec_time'] = round(time.time() - now, 2) 
 
-  return x.text
+  return result
 
+result = list(map(request_test, list(range(1000))))
+result = pd.DataFrame(result)
 
-
-
+print(result.info())
+result.to_csv('traffic_result.csv')
 
 
 
